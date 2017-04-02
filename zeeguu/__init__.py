247851f599738cd7252375097b80f5de3dab5b16
Mircea Lungu <mircea.lungu@gmail.com>
2017-04-02 18:44:08 +0200

    added logging

--- zeeguu/__init__.py
+++ zeeguu/__init__.py
@@ -1,3 +1,9 @@
 # -*- coding: utf8 -*-
+import os
+
+log_file = os.path.expanduser("~/.config/zeeguu/zeeguu_log.txt")
 
 
+def log(text):
+    with open(log_file, "a") as myfile:
+        myfile.write(text.encode('utf8')+"\n")
