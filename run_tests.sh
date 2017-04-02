d46e405647e450a5176fb97881c02e567427056c
Mircea Lungu <mircea.lungu@gmail.com>
2017-04-02 22:25:10 +0200

    failing test.

--- run_tests.sh
+++ run_tests.sh
@@ -2,4 +2,6 @@
 export PYTHONWARNINGS="ignore"
 export ZEEGUU_CORE_CONFIG='./testing_default.cfg'
 python -m unittest discover -v
+return_value=$?
 export PYTHONWARNINGS="default"
+exit $return_value
