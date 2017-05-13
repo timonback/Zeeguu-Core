341427182628985410c20fe368168059d6134c09
Mircea Lungu <mircea.lungu@gmail.com>
2017-05-13 21:49:22 +0200

    treading more careful around the possible errors here

--- zeeguu/model/user.py
+++ zeeguu/model/user.py
@@ -3,7 +3,7 @@ import random
 from sqlalchemy import Column, Table, ForeignKey, Integer
 import sqlalchemy.orm
 from sqlalchemy.orm import relationship
-
+from sqlalchemy.orm.exc import NoResultFound
 
 from zeeguu import util
 from zeeguu.model.bookmark import Bookmark
@@ -53,7 +53,7 @@ class User(db.Model):
         self.native_language = native_language or Language.default_native_language()
 
     @classmethod
-    def create_anonymous(cls, uuid, password, learned_language_code, native_language = None):
+    def create_anonymous(cls, uuid, password, learned_language_code, native_language_code = None):
         """
 
         :param uuid:
@@ -66,7 +66,17 @@ class User(db.Model):
         # since the DB must have an email we generate a fake one
         fake_email = uuid+ANONYMOUS_EMAIL_DOMAIN
 
-        new_user = cls(fake_email, uuid, password, learned_language=Language.find(learned_language_code), native_language=native_language)
+        try:
+            learned_language = Language.find(learned_language_code)
+        except NoResultFound as e:
+            learned_language = None
+
+        try:
+            native_language = Language.find(native_language_code)
+        except NoResultFound as e:
+            native_language = None
+
+        new_user = cls(fake_email, uuid, password, learned_language=learned_language, native_language=native_language)
 
         # # Until we find a better way of adding exercises for anonymous and new users... we simply
         # from zeeguu.temporary.default_words import default_bookmarks
