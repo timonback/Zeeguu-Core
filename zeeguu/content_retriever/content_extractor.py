6b5d49409d45ff12d96c601d7e7e14de454f7e9f
Mircea Lungu <mircea.lungu@gmail.com>
2017-05-14 15:44:12 +0200

    some logging stuff

--- zeeguu/content_retriever/content_extractor.py
+++ zeeguu/content_retriever/content_extractor.py
@@ -9,6 +9,7 @@ class ArticleContentExtractor:
     """
 
     def __init__(self, url, lang_code):
+        print("initializing article reader for {0}".format(url))
         self.article = Article(url=url, language=lang_code)
         self.article.download()
         self.article.parse()
@@ -18,5 +19,6 @@ class ArticleContentExtractor:
 
     @classmethod
     def worker(cls, url, lang_code, result):
+        print("in worker")
         extractor = cls(url, lang_code)
         result.put(dict(content=extractor.get_content(), image="", url=url))
