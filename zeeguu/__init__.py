d8a8c5aedad4414e598c9c923a1699196d5906d0
Mircea Lungu <mircea.lungu@gmail.com>
2017-05-14 14:39:24 +0200

    logging as utf-8

--- zeeguu/__init__.py
+++ zeeguu/__init__.py
@@ -5,5 +5,5 @@ log_file = os.path.expanduser("~/.config/zeeguu/zeeguu_log.txt")
 
 
 def log(text):
-    with open(log_file, "a") as myfile:
+    with open(log_file, "a", encoding='utf-8') as myfile:
         myfile.write((text+"\n"))
