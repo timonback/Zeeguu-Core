547e21ad07708d8c0edd80cdffbee0659bf163f0
Mircea Lungu <mircea.lungu@gmail.com>
2017-05-14 12:42:56 +0200

    fixed variable name overriding package name

--- zeeguu/content_retriever/parallel_retriever.py
+++ zeeguu/content_retriever/parallel_retriever.py
@@ -1,8 +1,8 @@
 
 import threading
-
 import time
 from zeeguu.content_retriever.content_extractor import ArticleContentExtractor
+import queue
 
 
 def get_content_for_urls(urls, lang_code, timeout = 10):
@@ -12,13 +12,13 @@ def get_content_for_urls(urls, lang_code, timeout = 10):
     :param timeout: seconds to wait for the contents
     :return:
     """
-    import queue
-    queue = queue.Queue()
+
+    art_queue = queue.Queue()
 
     # Start worker threads to get url contents
     threads = []
     for url in urls:
-        thread = threading.Thread(target=ArticleContentExtractor.worker, args=(url, lang_code, queue))
+        thread = threading.Thread(target=ArticleContentExtractor.worker, args=(url, lang_code, art_queue))
         thread.daemon = True
         threads.append(thread)
         thread.start()
@@ -31,7 +31,7 @@ def get_content_for_urls(urls, lang_code, timeout = 10):
     content_list = []
     for i in range(len(urls)):
         try:
-            content_list.append(queue.get_nowait())
+            content_list.append(art_queue.get_nowait())
         except queue.Empty:
             pass
 
