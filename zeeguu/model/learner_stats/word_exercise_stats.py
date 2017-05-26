09308ffbe393d524b0718ec9919a16fe61e54616
Timon Back <T.Back@student.rug.nl>
2017-05-26 12:21:51 +0200

    sqlalchemy

--- zeeguu/model/learner_stats/word_exercise_stats.py
+++ zeeguu/model/learner_stats/word_exercise_stats.py
@@ -3,6 +3,7 @@ import decimal
 import sqlalchemy.orm
 import zeeguu
 from zeeguu.algos.algo_service import AlgoService
+from zeeguu.model import User, UserWord
 
 db = zeeguu.db
 from zeeguu.model.bookmark import Bookmark
@@ -22,10 +23,10 @@ class ExerciseBasedProbability(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-    user = db.relationship("User", backref="user")
+    user = db.relationship(User)
 
     user_word_id = db.Column(db.Integer, db.ForeignKey('user_word.id'), nullable=False)
-    user_word = db.relationship("UserWord", backref="user_word")
+    user_word = db.relationship(UserWord)
 
     probability = db.Column(db.DECIMAL(10, 9), nullable=False)
 
