bef237b5861f8a2fb4b488faac6dfffe0e25a91b
Mircea Lungu <mircea.lungu@gmail.com>
2017-05-29 23:33:16 +0200

    switched to using watchmen for content retrieval and caching

--- zeeguu/language/retrieve_and_compute.py
+++ zeeguu/language/retrieve_and_compute.py
@@ -7,13 +7,17 @@ def retrieve_urls_and_compute_metrics(urls, language, user, timeout = 10):
     content_and_urls = get_content_for_urls(urls, language.id, timeout)
 
     for each in content_and_urls:
-        difficulty = user.text_difficulty(each['content'],language)
-        urls_and_metrics[each['url']] = {
-            'difficulty': {
-                'normalized':   difficulty['normalized'],
-                'discrete'  :   difficulty['discrete'],
-                'average'   :   difficulty['score_average']
+        try:
+            difficulty = user.text_difficulty(each['content'],language)
+            urls_and_metrics[each['url']] = {
+                'difficulty': {
+                    'normalized':   difficulty['normalized'],
+                    'discrete'  :   difficulty['discrete'],
+                    'average'   :   difficulty['score_average']
+                }
             }
-        }
+        except Exception as e:
+            print ("Failed while trying to compute difficulty for " + each['url'])
+            print (str(e))
 
     return urls_and_metrics
