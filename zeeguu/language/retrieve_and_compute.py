d52b30b010bbc6fb5ee59e12334b6e5f24b741d4
Mircea Lungu <mircea.lungu@gmail.com>
2017-05-29 23:33:16 +0200

    switched to using watchmen for content retrieval and caching

--- zeeguu/language/retrieve_and_compute.py
+++ zeeguu/language/retrieve_and_compute.py
@@ -8,22 +8,14 @@ def retrieve_urls_and_compute_metrics(urls, language, user, timeout = 10):
 
     for each in content_and_urls:
         try:
-
-            if len(each['content']) > 300:
-                # this 500 characters limit assumes that shorter
-                # articles are not actually "articles" but probably
-                # parser mistakes
-                difficulty = user.text_difficulty(each['content'],language)
-                urls_and_metrics[each['url']] = {
-                    'difficulty': {
-                        'normalized':   difficulty['normalized'],
-                        'discrete'  :   difficulty['discrete'],
-                        'average'   :   difficulty['score_average']
-                    }
+            difficulty = user.text_difficulty(each['content'],language)
+            urls_and_metrics[each['url']] = {
+                'difficulty': {
+                    'normalized':   difficulty['normalized'],
+                    'discrete'  :   difficulty['discrete'],
+                    'average'   :   difficulty['score_average']
                 }
-            else:
-                print ("ATTN!: since size is only {0} will not return {1}".
-                       format(len(each['content']), url))
+            }
         except Exception as e:
             print ("Failed while trying to compute difficulty for " + each['url'])
             print (str(e))
