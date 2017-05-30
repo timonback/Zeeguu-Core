d8683d3ef7f8e1460f01429e41df19cfdc52f2a2
Timon Back <T.Back@student.rug.nl>
2017-05-30 14:06:37 +0200

    Added Faker as python library

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setuptools.setup(
                       "feedparser",
                       "wordstats",
                       "requests",
-                      "newspaper3k"
+                      "newspaper3k",
+                      "Faker"
                       )
 )
