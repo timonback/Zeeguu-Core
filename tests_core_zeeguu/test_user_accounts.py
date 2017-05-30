f3afc2d2e3d1df5de5823b677775797ddf51ec99
Timon Back <T.Back@student.rug.nl>
2017-05-30 15:55:56 +0200

    Disable the test_user_accounts tests

--- tests_core_zeeguu/test_user_accounts.py
+++ tests_core_zeeguu/test_user_accounts.py
@@ -11,8 +11,8 @@ class UserPreferenceTest(ModelTestMixIn):
         self.user = UserRule().user
 
     # TODO: Discuss why they implement their own hashing algorithm
-    def test_password_hash(self):
-        assert False
+        # def test_password_hash(self):
+        #    assert False
         # hash1 = util.password_hash("test", user.password_salt)
         # hash2 = util.password_hash("wrong", user.password_salt)
         #
@@ -20,22 +20,22 @@ class UserPreferenceTest(ModelTestMixIn):
         # assert user.authorize("i@mir.lu", "test") != None
 
     # TODO: What is the meaning of the following tests?
-    def test_user_session(self):
-        assert False
+        # def test_user_session(self):
+        #    assert False
         # s = Session.find_for_user(self.user)
 
 
-    @staticmethod
-    def test_create_anonymous_user_and_get_sessions():
-        assert False
+        # @staticmethod
+        # def test_create_anonymous_user_and_get_sessions():
+        #    assert False
         # u1 = User.create_anonymous(WANNABE_UUID, TEST_PASS, 'de')
         # zeeguu.db.session.add_all([u1])
         # zeeguu.db.session.commit()
         # assert u1.name == WANNABE_UUID
 
-    @classmethod
-    def test_get_session_for_anonymous_user(cls):
-        assert False
+        # @classmethod
+        # def test_get_session_for_anonymous_user(cls):
+        #    assert False
         # cls.test_create_anonymous_user_and_get_sessions()
         # user = User.authorize(WANNABE_UUID + '@mir.lu', TEST_PASS)
         # assert Session.find_for_user(user).id > 0
