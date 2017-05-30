31104e24d9ffdb63e00690bd40cae49aa4528690
Timon Back <T.Back@student.rug.nl>
2017-05-30 16:15:50 +0200

    Fix test_words_to_study

--- tests_core_zeeguu/test_words_to_study.py
+++ tests_core_zeeguu/test_words_to_study.py
@@ -1,10 +1,10 @@
-import random
 from datetime import datetime
 
 import zeeguu
 
 from tests_core_zeeguu.model_test_mixin import ModelTestMixIn
 from tests_core_zeeguu.rules.exercise_rule import ExerciseRule
+from tests_core_zeeguu.rules.outcome_rule import OutcomeRule
 from tests_core_zeeguu.rules.user_rule import UserRule
 from zeeguu.algos.algo_service import AlgoService
 
@@ -13,8 +13,10 @@ class WordsToStudyTest(ModelTestMixIn):
     def setUp(self):
         super().setUp()
 
+        BOOKMARK_COUNT = 10
+
         self.user_rule = UserRule()
-        self.user_rule.add_bookmarks(random.randint(1, 3), exercises_count=1)
+        self.user_rule.add_bookmarks(BOOKMARK_COUNT, exercises_count=1)
         self.user = self.user_rule.user
 
     def test_new_bookmark_has_the_highest_priority(self):
@@ -37,14 +39,20 @@ class WordsToStudyTest(ModelTestMixIn):
         # Add an exercise
         exercise_rule = ExerciseRule()
         exercise_rule.exercise.time = datetime.now()
+        exercise_rule.exercise.solving_speed = 100
+        exercise_rule.exercise.outcome = OutcomeRule().correct
         first_bookmark_to_study.add_new_exercise(exercise_rule.exercise)
 
         AlgoService.update_bookmark_priority(zeeguu.db, self.user)
 
         # THEN
-        bookmark = self.__get_bookmark_with_highest_priority()
-        assert first_bookmark_to_study != bookmark
+        bookmark = self.__get_bookmark_with_lowest_priority()
+        assert first_bookmark_to_study == bookmark
 
     def __get_bookmark_with_highest_priority(self):
         bookmarks_to_study = self.user.bookmarks_to_study()
         return bookmarks_to_study[0]
+
+    def __get_bookmark_with_lowest_priority(self):
+        bookmarks_to_study = self.user.bookmarks_to_study()
+        return bookmarks_to_study[-1]
