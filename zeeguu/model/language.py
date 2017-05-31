fa3ba44aa8327e8db805d693c0ca42ebd69e74a7
Mircea Lungu <mircea.lungu@gmail.com>
2017-05-31 02:06:59 +0200

    .

--- zeeguu/model/language.py
+++ zeeguu/model/language.py
@@ -30,7 +30,7 @@ class Language(db.Model):
 
     @classmethod
     def native_languages(cls):
-        return [cls.find("en")]
+        return [cls.find("en"), cls.find("nl")]
 
     @classmethod
     def available_languages(cls):
