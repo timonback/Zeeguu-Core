9d63f22cddd2d60cffb0666d6811f7da0397ea05
Mircea Lungu <mircea.lungu@gmail.com>
2017-06-04 17:42:18 +0200

    script for fixing translations

---
+++ tools/migrations/towards_single_translation.py
@@ -0,0 +1,18 @@
+import zeeguu
+from zeeguu.model import Bookmark, UserWord
+session = zeeguu.db.session
+
+all_bs = Bookmark.query.filter_by(translation_id=None).all()
+fixed = []
+i = 0
+for b in all_bs:
+    i+= 1
+    fixed.append(b.id)
+    b.translation = b.translations_list[0]
+    session.add(b)
+    if i == 100:
+        print ("fixed form: {0} to {1}".
+        	format(fixed[0],fixed[-1])) 
+        i = 0
+        fixed = []
+        session.commit()
