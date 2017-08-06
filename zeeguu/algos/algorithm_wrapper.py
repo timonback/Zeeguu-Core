d88bbc35e1125482afbb9dab6f116c61cb686984
Timon Back <T.Back@student.rug.nl>
2017-08-06 17:22:24 +0200

    Improved database linking

--- zeeguu/algos/algorithm_wrapper.py
+++ zeeguu/algos/algorithm_wrapper.py
@@ -52,11 +52,11 @@ class AlgorithmSDCaller(AlgorithmWrapper):
 
         """
         N = max_iterations - exercise.id
-        alpha = exercise.outcome.correct
+        alpha = not exercise.outcome.correct
         sd = self.convert_rt_to_sd(exercise)
 
         return N, alpha, sd
 
     def convert_rt_to_sd(self, exercise):
-        exercise_stats = ExerciseStats.query().filter_by(exercise_source_id=exercise.source_id).one()
+        exercise_stats = ExerciseStats.query.filter_by(exercise_source=exercise.source).one()
         return (exercise.solving_speed - exercise_stats.mean) / exercise_stats.sd
